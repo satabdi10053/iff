9f9cddb8897916e198089369a4348de75d596d14
satabdi10053 <45042916+satabdi10053@users.noreply.github.com>
2026-05-14 23:39:40 +0200

    Create bronze_providers.sql

---
+++ bronze_providers.sql
@@ -0,0 +1,14 @@
+CREATE TABLE IF NOT EXISTS providers_raw (
+  provider_id       INT,
+  name              STRING,
+  location_city     STRING,
+  location_country  STRING,
+  generation_date   STRING,
+  batch_number      INT
+)
+USING CSV
+OPTIONS (
+  header = 'true',
+  inferSchema = 'false'
+)
+LOCATION 'dbfs:/mnt/raw/providers/';
